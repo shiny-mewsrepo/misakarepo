d26da76eaa7ffb13c3c7a4e549d0905cc42d1554
lushaosalt <121482361+lushaosalt@users.noreply.github.com>
2023-05-28 00:05:57 +0300

    Add files via upload

---
+++ imagehost/app/rector.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Chevereto.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevereto.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__
+    ]);
+    $rectorConfig->sets([SetList::DEAD_CODE, SetList::PHP_80]);
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+};
